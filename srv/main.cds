f4cf24cbec4221cba60601c183082f9efbf0ca43
Mithunraj V R <vrmithunraj@gmail.com>
2025-12-08 09:19:15 +0000

    Initial commit

--- srv/main.cds
+++ srv/main.cds
@@ -1,16 +1,17 @@
 using org from '../db/schema';
-
-
-extend org.qmarco.Books with{
-    virtual urgency:String;
+extend org.qmarco.Books with {
+    virtual urgency : String;
 }
- service bookshop {
-entity Books as  projection on org.qmarco.Books actions {
-    function stockValue() returns Integer;
-}
-entity Authors as projection on org.qmarco.Authors;
-entity Orders as projection on org.qmarco.Orders;
+service bookshop {
+    entity Books   as projection on org.qmarco.Books
+        actions {
+            function stockValue() returns Integer;
+            action   setPrice(price: Integer)
+        };
 
-function totalStock() returns Integer
-   
+    entity Authors as projection on org.qmarco.Authors;
+    entity Orders  as projection on org.qmarco.Orders;
+
+    function totalStock() returns Integer
+
 }
--- srv/main.js
+++ srv/main.js
@@ -1,36 +1,41 @@
 const cds = require('@sap/cds')
-const { loggers } = require('@sap/cds/lib/log/cds-log')
-const { SELECT } = require('@sap/cds/lib/ql/cds-ql')
+const { SELECT, UPDATE } = require('@sap/cds/lib/ql/cds-ql')
 const logger = cds.log('projects')
 const { Books } = cds.entities('bookshop')
 const totalStock = 'totalStock'
-
 module.exports = cds.service.impl(function () {
-
-
   const changeUrgencyDueToSubject = data => {
     const books = Array.isArray(data) ? data : [data]
-
     for (let b of books) {
       if (b.title?.toLowerCase().includes("harmless")) {
         b.urgency = "HIGH"
       }
-
     }
   }
   this.on(totalStock, async () => {
-   const result = await SELECT.one.from(Books).columns('sum(stock) as total')
-
-    return result.total  
-    
+    const result = await SELECT.one.from(Books).columns('sum(stock) as total')
+    return result.total
   })
-this.after('READ',Books,changeUrgencyDueToSubject)
+  this.after('READ', Books, changeUrgencyDueToSubject)
+  this.on('stockValue', Books, async (req) => {
 
- this.on('stockValue',Books,async({params:[id]}) =>{
- const result=  await SELECT.columns(['stock * price as stockValue'])
-  .from (Books)
-  .where`ID = ${id}`
-   return result.stockValue
- })
+    const { ID } = req.params[0];
+
+    const result = await SELECT.one
+      .columns`stock * price as stockValue`
+      .from(Books)
+      .where({ ID });
+
+    return { value: result?.stockValue ?? null };
+  });
+  this.on('setPrice',Books,async req =>{
+    const id = req.params[0]
+    logger(req.data)
+    await UPDATE (Books, id).with({
+      price:req.data.params
+    })
+
+    return await SELECT(Books,id)
+  })
+
 })
- 
